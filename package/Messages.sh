8f6968bf8a6e7b3b037271a425de3fd9eeb315b4
Andreas Krutzler <andi.krutzler@gmail.com>
2017-12-30 17:46:13 +0100

    Fixed typo.

--- package/Messages.sh
+++ package/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.js -o -name \*.qml` -o $podir/plasma_applet_org.kde.plasma.volume.pot
+$XGETTEXT `find . -name \*.js -o -name \*.qml` -o $podir/plasma_applet_org.kde.plasma.audiodeviceswitcher.pot
 rm -f rc.cpp
